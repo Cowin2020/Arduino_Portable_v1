7eaf78805b37e3547bb5fe194b033b66d06d0045
Lee Chun Kin <leeck123@hku.hk>
2024-04-11 15:33:09 +0800

    Move styles of SD card webpage to CSS file

--- SDCard/script.js
+++ SDCard/script.js
@@ -6,14 +6,11 @@ function $T(string) {
 	return document.createTextNode(string);
 }
 
-function $E(name, attributes, styles, children) {
+function $E(name, attributes, children) {
 	var element = document.createElementNS(document.documentElement.namespaceURI, name);
 	if (attributes != null)
 		for (var name in attributes)
 			element.setAttribute(name, attributes[name]);
-	if (styles != null)
-		for (var name in styles)
-			element.style[name] = styles[name];
 	if (Array.isArray(children))
 		children.forEach(
 			function (child) {
@@ -28,92 +25,44 @@ document.body.style["margin"] = "1ex";
 var $dashboard;
 
 document.body.appendChild(
-	$dashboard = $E("div",
-		{"id": "dashboard"},
-		{
-			"margin": "2ex",
-			"border": "double 8px #FC8",
-			"border-radius": "4ex",
-			"padding": "2ex",
-			"width": "calc(100vw - 10ex - 16px)",
-			"background-color": "#8AC",
-		}
-	)
+	$dashboard = $E("div", {"id": "dashboard"})
 );
 
 function show_dashboard(row) {
 	$dashboard.textContent = "";
 	if (row == null)
 		$dashboard.appendChild(
-			$E("div",
-				null,
-				{
-					"text-align": "center",
-					"font-size": "4rem",
-					"color": "#000",
-					"text-shadow": "2px 2px 4px #FFF"
-				},
-				[$T("No data")]
-			)
+			$E("div", {"class": "nodata"}, [
+				$T("No data")
+			])
 		);
 	else {
 		$dashboard.appendChild(
-			$E("div",
-				null,
-				{
-					"display": "flex",
-					"flex-direction": "row",
-					"flex-wrap": "wrap",
-					"justify-content": "center",
-					"column-gap": "1.5ex",
-					"margin": "1ex",
-					"text-align": "center",
-					"font-size": "6ex",
-					"color": "#CFD"
-				},
+			$E("div", {"class": "title"},
 				row[0].split("T").map(
 					function (s) {
-						return $E("span", null, null, [$T(s)]);
+						return $E("span", null, [$T(s)]);
 					}
 				)
 			)
 		);
 		function $item(title, index, unit) {
-			return $E("div",
-				null,
-				{
-					"display": "flex",
-					"flex-flow": "column wrap",
-					"justify-content": "center",
-					"margin": "0.5rem",
-					"border": "solid thin lightyellow",
-					"padding": "1rem",
-					"text-align": "center"
-				},
-				[
-					$E("span", null, {"font-size": "2rem", "color": "#FED"}, [$T(title)]),
-					$E("span", null, {"font-size": "3rem", "color": "snow"}, [
-						$T(row[index]),
-						$E("span", null, {"font-size": "1.5rem"}, [$T(unit)])
-					])
-				]
-			)
+			return $E("div", {"class": "item"}, [
+				$E("span", {"class": "name"}, [
+					$T(title)
+				]),
+				$E("span", {"class": "value"}, [
+					$T(row[index]),
+					$E("span", null, [$T(unit)])
+				])
+			])
 		}
 		$dashboard.appendChild(
-			$E("div",
-				null,
-				{
-					"display": "flex",
-					"flex-flow": "row wrap",
-					"justify-content": "center",
-					"column-gap": "3ex"
-				},
-				[
-					$item("Temperature", 1, "\u2103"),
-					$item("Pressure", 2, "Pa"),
-					$item("Humidity", 3, "%")
-				]
-			)
+			$E("div", {"class": "items"}, [
+				$item("Temperature", 1, "\u2103"),
+				$item("Pressure", 2, "Pa"),
+				$item("Humidity", 3, "%")
+			])
 		);
 	}
 }
@@ -121,86 +70,40 @@ function show_dashboard(row) {
 show_dashboard(null);
 
 document.body.appendChild(
-	$E("p",
-		null,
-		{
-			"display": "flex",
-			"flex-flow": "row wrap",
-			"text-align": "center"
-		},
-		[
-			$E("a",
-				{"href": "setting.html"},
-				{
-					"margin": "1ex",
-					"border": "solid thin gray",
-					"padding": "1ex"
-				},
-				[$T("Settings")]
-			),
-			$E("a",
-				{
-					"href": "recent.csv",
-					"download": ""
-				},
-				{
-					"margin": "1ex",
-					"border": "solid thin gray",
-					"padding": "1ex"
-				},
-				[$T("Download recent data")]
-			),
-			$E("a",
-				{
-					"href": "all.csv",
-					"download": ""
-				},
-				{
-					"margin": "1ex",
-					"border": "solid thin gray",
-					"padding": "1ex"
-				},
-				[$T("Download all data")]
-			)
-		]
-	)
+	$E("p", {"class": "download-links"}, [
+		$E("a", {"href": "setting.html"}, [
+			$T("Settings")
+		]),
+		$E("a", {"href": "recent.csv", "download": ""}, [
+			$T("Download recent data")
+		]),
+		$E("a", {"href": "all.csv", "download": ""}, [
+			$T("Download all data")
+		])
+	])
 );
 
 var $reflesh, $auto;
 
 document.body.appendChild(
-	$reflesh = $E("form",
-		null,
-		{
-			"margin-top": "2ex",
-			"margin-bottom": "2ex"
-		},
-		[
-			$E("button", {"type": "submit"}, null, [$T("Reflesh now")]),
-			$E("label",
-				null,
-				{
-					"margin-left": "2ex",
-					"padding": "1ex"
-				},
-				[$T("Auto reflesh")]
-			),
-			$auto = $E("input", {"type": "checkbox"}, null, null)
-		]
-	)
+	$reflesh = $E("form", {"class": "reflesh"}, [
+		$E("button", {"type": "submit"}, [$T("Reflesh now")]),
+		$E("label", null, [$T("Auto reflesh")]),
+		$auto = $E("input", {"type": "checkbox"})
+	])
 );
 
 var $plot_temperature, $plot_pressure, $plot_humidity;
 
 void function () {
 	document.body.appendChild(
-		$plot_temperature = $E("div", null, {"width": "100%", "height": "90vh"})
+		$plot_temperature = $E("div", {"class": "plot"})
 	);
 	document.body.appendChild(
-		$plot_pressure = $E("div", null, {"width": "100%", "height": "90vh"})
+		$plot_pressure = $E("div", {"class": "plot"})
 	);
 	document.body.appendChild(
-		$plot_humidity = $E("div", null, {"width": "100%", "height": "90vh"})
+		$plot_humidity = $E("div", {"class": "plot"})
 	);
 }();
 
@@ -254,13 +157,13 @@ function show_plot(rows) {
 var $list;
 
 document.body.appendChild(
-	$E("table", null, {"width": "100%", "border-collapse": "collapse"}, [
-		$E("thead", null, {"border-bottom-style": "solid"}, [
-			$E("tr", null, null, [
-				$E("th", null, null, [$T("Time")]),
-				$E("th", null, null, [$T("Temperature")]),
-				$E("th", null, null, [$T("Pressure")]),
-				$E("th", null, null, [$T("Humidity")])
+	$E("table", null, [
+		$E("thead", null, [
+			$E("tr", null, [
+				$E("th", null, [$T("Time")]),
+				$E("th", null, [$T("Temperature")]),
+				$E("th", null, [$T("Pressure")]),
+				$E("th", null, [$T("Humidity")])
 			])
 		]),
 		$list = $E("tbody")
@@ -298,18 +201,10 @@ function load() {
 		for (var i = 0; rows.length > i; ++i) {
 			var fields = rows[rows.length - i - 1];
 			$list.appendChild(
-				$E("tr", null, null,
+				$E("tr", null,
 					fields.map(
 						function (field) {
-							return $E("td",
-								null,
-								{
-									"border-style": "solid",
-									"border-width": "thin",
-									"text-align": "center"
-								},
-								[$T(field)]
-							);
+							return $E("td", null, [$T(field)]);
 						}
 					)
 				)
